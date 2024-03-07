300609cc67b3298c0be937db5ac05ee420f3b078
Escarsel <eloisa.scarsella@nttdata.com>
2024-03-07 10:28:08 +0100

    Fix import

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Box, Grid } from "@mui/material";
-import HomeCardComponent from "../components/CardComponents/HomeCardComponent";
-import { homePageCard } from "../utils/homePageCard";
-import { HomePageTitle } from "../components/TitleComponents/HomePageTitle";
+// import HomeCardComponent from "../components/CardComponents/HomeCardComponent";
+// import { homePageCard } from "../utils/homePageCard";
+// import { HomePageTitle } from "../components/TitleComponents/HomePageTitle";
 import BoxPageLayout from "./Layout/BoxPageLayout";
 
 
@@ -10,14 +10,14 @@ export default function HomePage () {
 	return(
 		<BoxPageLayout px={15}>
 			<Grid container spacing={0.5}>
-				<HomePageTitle
+				{/* <HomePageTitle
 					title={"Console management"}
 					subTitle={"Console per la gestione delle risorse"}
-				/>
+				/> */}
 			</Grid>
 			<Box  my="8%">
 				<Grid container spacing={8} >
-					{
+					{/* {
 						homePageCard.filter(el=> el.id!=="home").map((e, i) => (
 							<Grid item xs={4} mx={"auto"} key={e.title}>
 								<HomeCardComponent
@@ -28,7 +28,7 @@ export default function HomePage () {
 								/>
 							</Grid>
 						))
-					}
+					} */}
 				</Grid>
 			</Box>
 		</BoxPageLayout>
