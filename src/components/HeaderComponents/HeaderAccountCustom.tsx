23d9ccea1189892ccc744ed3656c2321aa0bc06b
Escarsel <eloisa.scarsella@nttdata.com>
2024-03-14 19:05:07 +0100

    Aggiunto bottone home nell'header + fix margin

--- src/components/HeaderComponents/HeaderAccountCustom.tsx
+++ src/components/HeaderComponents/HeaderAccountCustom.tsx
@@ -1,11 +1,14 @@
-import { Container, Button, Stack, Typography } from "@mui/material";
+import { Container, Button, Stack, Typography, Box, useTheme, ButtonBase } from "@mui/material";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
-import { Box } from "@mui/system";
 import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Ctx } from "../../DataContext";
 import { RootLinkType } from "../model/UserModel";
 import { USER_EMAIL } from "../../commons/endpoints";
 import { fetchRequest } from "../../hook/fetch/fetchRequest";
+import IconBox from "../Commons/IconBox";
+import ActionIcon from "../CustomTextField/ActionIcon";
+import ROUTES from "../../routes";
 
 type HeaderAccountProps = {
 	rootLink: RootLinkType;
@@ -22,6 +25,8 @@ export const HeaderAccountCustom = ({
 	const { userEmail, setUserEmail, abortController } = useContext(Ctx);
 
 	const token = sessionStorage.getItem("jwt_console");
+	const navigate = useNavigate();
+	const theme = useTheme();
 
 	const getTokenEmail = async () => {
 		try {
@@ -49,18 +54,41 @@ export const HeaderAccountCustom = ({
 				borderBottom: 1,
 				borderColor: "divider",
 				backgroundColor: "background.paper",
-				minHeight: "60px",
+				minHeight: "40px",
 			}}
 		>
 			<Container maxWidth={false}>
 				<Stack
 					spacing={2}
 					direction="row"
-					justifyContent="space-between"
+					// justifyContent="space-between"
 					alignItems="center"
 				>
 					<Box pl={3} className="logo" aria-label={rootLink?.ariaLabel} title={rootLink?.title} display={"flex"} flexDirection={"row"} alignItems={"center"}>
 						{rootLink?.element}
+					</Box>
+					<Box pl={2} mb={2} className="home" aria-label={"buttonHome"} display={"flex"} flexDirection={"row"} alignItems={"center"} justifyContent={"flex-start"}>
+						{/* <Button variant="text" size="medium" >Text</Button> */}
+						{/* <ActionIcon icon={"Launch"} color={theme.palette.text.primary} pad={1.5} size={"1em"}  /> */}
+						<Button
+							variant="text"
+							size="large"
+							onClick={()=>navigate(ROUTES.HOME)}
+							sx={{
+								padding: "0px 0px 0px 8px",
+								fontSize:"1.5em",
+								marginBottom: "5px",
+								// color: theme.palette.text.primary,
+								"&:hover": {
+									backgroundColor: "transparent",
+									textDecoration:"underline"
+								},
+							}}
+							startIcon={<IconBox icon={"HomeOutlined"} color={theme.palette.primary.main} pad={0.5} size={"1em"} marg={"5px 0px 0px 0px"} />}
+						>
+		Home
+							
+						</Button>
 					</Box>
 
 					<Stack
