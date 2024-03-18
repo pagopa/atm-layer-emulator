4eafd7b1ec7fdb4c2d0b5d9050d16b57c631223d
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-03-18 14:50:04 +0100

    correcting next endpoint

--- src/commons/endpoints.ts
+++ src/commons/endpoints.ts
@@ -1,3 +1,3 @@
 export const USER_EMAIL = "/info/email";
 export const TASK_MAIN = "/task/main";
-export const TASK_NEXT = "/next/trns/:transactionId";
+export const TASK_NEXT = "/task/next/trns/:transactionId";
