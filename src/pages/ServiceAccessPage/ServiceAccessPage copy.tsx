357cdeafc49aa913fb34724a92ed7b509bcda911
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-03-21 15:34:22 +0100

    removing unused commons, adding style pages

--- src/pages/ServiceAccessPage/ServiceAccessPage copy.tsx
+++ src/pages/ServiceAccessPage/ServiceAccessPage copy.tsx
@@ -7,7 +7,7 @@ import { Ctx } from "../../DataContext";
 import { decodeRenderHtml } from "../../components/DecodeRenderHtml/decodeRenderHtml";
 import { TASK_NEXT } from "../../commons/endpoints";
 import { fetchRequest } from "../../hook/fetch/fetchRequest";
-import "./css/ServiceAccess.css";
+import "./css/CommonStyle.css";
 // import { createMuiRow, wrapElementInMuiCol, wrapElementInMuiColAndRow, wrapElementInMuiRow } from "../../utils/Commons";
 import { executeCommand } from "../../commons/utilsFunctions";
 
