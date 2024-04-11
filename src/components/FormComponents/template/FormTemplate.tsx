5a1b494c27ab806469ac1ff9bdb8e347ae429c58
Gabriele Maiocchi <fmaiocch@emeal.nttdata.com>
2024-04-11 09:37:31 +0200

    made some fix

--- src/components/FormComponents/template/FormTemplate.tsx
+++ src/components/FormComponents/template/FormTemplate.tsx
@@ -25,7 +25,7 @@ export default function FormTemplate({ handleSubmit, children, loadingButton }: 
 	// console.log(window.innerWidth>theme.breakpoints.values.md);
 	return (
 		<Box sx={{ maxWidth: window.innerWidth > theme.breakpoints.values.md ? "75%" : "100%" }}>
-			<Box p={3} my={3} mx={"auto"} sx={inputGroupStyle}  >
+			<Box p={3} my={2} mx={"auto"} sx={inputGroupStyle}  >
 				<Grid container >
 					
 
