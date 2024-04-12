f0d826d6536017055d3ada6f1d0272d1156444a6
EmVal <emanuele.valentini@nttdata.com>
2024-04-12 14:47:53 +0200

    App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-	// render(<App />);
-	// const linkElement = screen.getByText(/learn react/i);
-	// expect(linkElement).toBeInTheDocument();
+describe ("App Tests", () => {
+	test("Test App without jwt in sessionStorage", () => {
+
+		sessionStorage.setItem("tempLog", "fakeToken");
+		sessionStorage.setItem("jwt_emulator", "fakeToken");
+		sessionStorage.setItem("debugOn", "debugOn");
+	
+		render(
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		);
+	});
+	
+	test("Test App with jwt in sessionStorage", () => {
+	
+		sessionStorage.setItem("tempLog", "fakeToken");
+		sessionStorage.setItem("jwt_emulator", "fakeToken");
+		sessionStorage.setItem("debugOn", "debugOn");
+		localStorage.setItem("jwt_console", "prova");
+		localStorage.setItem("debugOn", "prova");
+	
+		render(
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		);
+	});
 });
