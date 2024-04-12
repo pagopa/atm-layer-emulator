0cd100f9e7d08f4fdd24698b3dee84193924a0ff
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-04-12 17:16:49 +0200

    changing login title

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -49,10 +49,10 @@ const LoginPage = () => {
 					backgroundColor: theme?.palette?.background?.paper,
 				}}>
 					<Box mb={4} display={"flex"} justifyContent={"center"}>
-						<Typography variant="h1" sx={{textAlign: "center", paddingRight: 0}}>Accedi alla console</Typography>
+						<Typography variant="h1" sx={{textAlign: "center", paddingRight: 0}}>Accedi all&apos;emulator</Typography>
 					</Box>
 					<Box mb={4} display={"flex"} justifyContent={"center"}>
-						<Typography variant="h6" sx={{textAlign: "center", paddingRight: 0}}>Lo spazio dedicato alla gestione dei processi ATM Layer</Typography>
+						<Typography variant="h6" sx={{textAlign: "center", paddingRight: 0}}>Lo spazio dedicato alla simulazione dei processi ATM Layer</Typography>
 					</Box>
 					<Box px={6} py={3}>
 						<Button
