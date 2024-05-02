28cb4a32f2c967b56a6ee8835ef536a8e3bb7ed2
LuciaM1 <144435187+LuciaM1@users.noreply.github.com>
2024-05-02 11:40:52 +0200

    Fix/get commands (#33)
    Co-authored-by: EmVal <emanuele.valentini@nttdata.com>

--- src/commons/utilsFunctions.ts
+++ src/commons/utilsFunctions.ts
@@ -5,11 +5,14 @@ import { AUTHORIZE, END, GET_IBAN, GET_PAN, PRINT_RECEIPT, NEXT, SCAN_BILL_DATA,
 
 export function executeCommand(driver: string, setCommand: any, next: any, responseProcess: any, ibanList?:IbanListDto, panInfo?:PanInfoDto) {
 
+	const outcomeVar = responseProcess?.task?.outcomeVarName;
+	const outcomeKey = outcomeVar || "result";
+	
 	switch (driver) {
 	case SCAN_BILL_DATA:
 
 		const nextScanBillData: any = (inputElement: HTMLInputElement) => {
-			next({ "result": "OK", "scanData": inputElement.value });
+			next({ [outcomeKey]: "OK", "scanData": inputElement.value });
 			setCommand("");
 		};
 
@@ -76,9 +79,9 @@ export function executeCommand(driver: string, setCommand: any, next: any, respo
 				doubtfulButton.textContent = "Esito Dubbio";
 				cancelButton.textContent = "Autorizzazione negata";
 		
-				authorizeButton.addEventListener("click", () => {next({ "result" : ESITO_OK, "continue" : true }); setCommand("");});
-				doubtfulButton.addEventListener("click", () => {next({ "result" : ESITO_DUBBIO, "continue" : true }); setCommand("");});
-				cancelButton.addEventListener("click", () => {next({ "result" : ESITO_KO}); setCommand("");});
+				authorizeButton.addEventListener("click", () => {next({ [outcomeKey] : ESITO_OK, "continue" : true }); setCommand("");});
+				doubtfulButton.addEventListener("click", () => {next({ [outcomeKey] : ESITO_DUBBIO, "continue" : true }); setCommand("");});
+				cancelButton.addEventListener("click", () => {next({ [outcomeKey] : ESITO_KO}); setCommand("");});
 	
 				const buttonsDiv = document.createElement("div");
 				buttonsDiv.setAttribute("id", "command-buttons-div");
@@ -112,7 +115,7 @@ export function executeCommand(driver: string, setCommand: any, next: any, respo
 					style.innerText = "body {border: 2px black solid;display: flex;flex-direction: column;width: 600px;margin-left:auto;margin-right:auto;font-family: Courier New, monospace; font-size: 23px;} p {display: flex;flex-wrap: wrap;margin-bottom: 0px; margin-left: 25px;margin-right: 25px;} .left {text-align: left; } .right {text-align: right; } .center {text-align: center; } .bold {font-weight: bold;} span{flex: 1; white-space: pre-line;-ms-word-wrap: break-word;}span:hover{border: 1px black dotted; } span.col-1 {flex: 0 0 8.33%; } span.col-2 {flex: 0 0 16.67%; } span.col-3 {flex: 0 0 25%; } span.col-4 {flex: 0 0 33.33%; } span.col-5 {flex: 0 0 41.67%; } span.col-6 {flex: 0 0 50%; } span.col-7 {flex: 0 0 58.33%; } span.col-8 {flex: 0 0 66.67%;} span.col-9 {flex: 0 0 75%;} span.col-10 {flex: 0 0 83.33%;} span.col-11 {flex: 0 0 91.67%;} span.col-12 {flex: 0 0 100%;}";
 					wnd.document.head.appendChild(style);
 
-					void next({ "result": "OK" });
+					void next({ [outcomeKey]: "OK" });
 				} else {
 					console.error("Impossibile aprire la finestra pop-up");
 				}
@@ -124,13 +127,21 @@ export function executeCommand(driver: string, setCommand: any, next: any, respo
 		history.back();
 		break;
 	case NEXT:
-		void next({ "result": "OK" });
+		void next({ [outcomeKey]: "OK" });
 		break;
 	case GET_IBAN:
-		void next({ "result": "OK", ...ibanList});
+		if(ibanList && ibanList.IBANlist.length > 0) {
+			void next({ [outcomeKey]: "OK", ...ibanList});
+		} else {
+			void next({ [outcomeKey]: "KO" });
+		}
 		break;
 	case GET_PAN:
-		void next({ "result": "OK", ...panInfo});
+		if(panInfo && panInfo.panInfo.length > 0) {
+			void next({ [outcomeKey]: "OK", ...panInfo});
+		} else {
+			void next({ [outcomeKey]: "KO" });
+		}
 		break;
 	default: return "";
 	};
