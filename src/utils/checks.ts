a3b6e7f2de1c50b19706ec3aed079b1719f95105
EmVal <emanuele.valentini@nttdata.com>
2024-05-31 15:19:44 +0200

    Fix Regex

--- src/utils/checks.ts
+++ src/utils/checks.ts
@@ -7,10 +7,6 @@ const checks = () => {
         (typeof field === "number" && field < 0)
 	);
 
-	// const isValidFiscalCode = (e: string) => {
-	// 	const fiscalCodeRegex = /^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/;
-	// 	return fiscalCodeRegex.test(e);
-	// };
 
 	const cfIsValid = (value: string) => {
 		
